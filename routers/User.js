8a605743a623680d7549046cefc1c60d7dc5110b
collins mbathi <perezcoolins@gmail.com>
2022-07-05 22:51:30 +0300

    last update

--- routers/User.js
+++ routers/User.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const  {login,logout,register,resetPassword,forgotPassword,getUser,getAllUsers,activateEmail,updateUser,deleteUser, VertifyPassword} = require('../controllers/User');
-const authAdmin = require('../middlewares/authAdmin');
-const auth = require('../middlewares/auth');
+// const authAdmin = require('../middlewares/authAdmin');
+// const auth = require('../middlewares/auth');
 
 
 router.post('/register',register);
@@ -9,12 +9,12 @@ router.get('/:id/verify/:token',activateEmail);
 router.post('/login',login);
 // router.post('/refresh',getAccessToken);
 router.post('/forgot',forgotPassword);
-router.post('/reset',auth,resetPassword);
-router.get('/information',auth,getUser);
-router.get('all_information',auth,authAdmin,getAllUsers);
+// router.post('/reset',auth,resetPassword);
+router.get('/information',getUser);
+router.get('/all_information',getAllUsers);
 router.get('/logout',logout);
-router.patch('/update',auth,updateUser);
-router.delete('/delete/:id',auth,authAdmin,deleteUser);
+router.patch('/update',updateUser);
+router.delete('/delete/:id',deleteUser);
 router.get('/:id/:token',VertifyPassword);
 router.post('/:id/:token',resetPassword);
 router.post('/password-reset',forgotPassword);
