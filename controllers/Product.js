fcdfbffd89dea4e3350ced46fce81ba55eee9e84
collins mbathi <perezcoolins@gmail.com>
2022-07-07 07:44:09 +0300

    fixed the pagination

--- controllers/Product.js
+++ controllers/Product.js
@@ -36,8 +36,8 @@ class APIfeatures {
     }
 
     paginating(){
-        const page = this.queryString.page * 1 || 1
-        const limit = this.queryString.limit * 1 || 9
+        const page = this.queryString.page * 1 || 3
+        const limit = this.queryString.limit * 1 || 20
         const skip = (page - 1) * limit;
         this.query = this.query.skip(skip).limit(limit)
         return this;
